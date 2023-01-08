c3dae4528749a14f9d5581c1892956144e1509a5
himanshukandpal-28 <himkandpal28@gmail.com>
2023-01-08 22:49:51 +0530

    Refractor

---
+++ whatsapp/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAQwmuusqbHldqaWo9xkoha9ZC-bsLGyG0",
+    authDomain: "whatsapp-clone-ab0cd.firebaseapp.com",
+    projectId: "whatsapp-clone-ab0cd",
+    storageBucket: "whatsapp-clone-ab0cd.appspot.com",
+    messagingSenderId: "872104876112",
+    appId: "1:872104876112:web:a8b056efa62b644708d955",
+    measurementId: "G-4CNE1K3JMZ"
+  };
